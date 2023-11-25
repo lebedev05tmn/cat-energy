4b73748702e6b7fbe4edb4999120ea51e3f644ab
lebedev05tmn <lebedev05tmn@gmail.com>
2023-11-25 22:30:35 +0300

    final

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,18 @@
+let burgerButton = document.querySelector(".burger");
+let siteMenu = document.querySelector(".site-menu");
+let closeButton = document.querySelector(".close");
+
+burgerButton.onclick = function () {
+    siteMenu.classList.add("site-menu--mobile");
+    burgerButton.classList.add("visually-hidden");
+    closeButton.classList.remove("visually-hidden");
+};
+
+closeButton.onclick = function () {
+    siteMenu.classList.remove("site-menu--mobile");
+    burgerButton.classList.remove("visually-hidden");
+    closeButton.classList.add("visually-hidden");
+};
 let togglerButton = document.querySelector(".slider__click");
 let togglerBlock = document.querySelector(".slider__toggler");
 let imageFirst = document.querySelector(".slider__picture");
